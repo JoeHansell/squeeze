fbd8397fd35cb45a83b849b562b80b52a801f288
JoeHansell <joseph.w.hansell@gmail.com>
2025-06-26 18:18:09 +0000

    Added 'utils' file with 'sum_overflows' functions

---
+++ src/utils.hpp
@@ -0,0 +1,15 @@
+#include <limits>
+#include <concepts>
+#include <cstdint>
+
+template<std::unsigned_integral T>
+bool sum_overflows(const T lhs, const T rhs)
+{
+    return std::numeric_limits<T>::max() - lhs < rhs;
+}
+
+template<std::unsigned_integral T, std::unsigned_integral... T_Pack>
+bool sum_overflows(const T first, const T second, const T_Pack... others)
+{
+    return sum_overflows(first, second) or sum_overflows(static_cast<T>(first + second), others...);
+}
